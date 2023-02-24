e284df160b0a71dd64f39fa8cffc72f2e51bcd81
ilyab <i-baykov@internet.ru>
2023-02-24 22:50:27 +0300

    Добавил Redirect

--- horoscope/url.py
+++ horoscope/url.py
@@ -3,5 +3,6 @@ from horoscope import views as vs
 
 urlpatterns = [
     path('menu', vs.test),
-    path("<str:current_sign>", vs.current_sign_info_str),
+    path("<int:current_sign_int>", vs.current_sign_info_int),
+    path("<str:current_sign>", vs.current_sign_info_str, name="current_sign_url"),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 from horoscope.support_cls import *
 from horoscope.support_function_views import *
 
 all_signs_dict = ZodiacSign.all_sign
+all_signs_list = list(ZodiacSign.all_sign)
 print(all_signs_dict)
 
 
@@ -17,3 +19,13 @@ def current_sign_info_str(request, current_sign):
     validation = all_signs_dict.get(current_sign)
     if validation:
         return display(current_sign, validation)
+    else:
+        return HttpResponseNotFound(f"Извините, но мы не знаем такой знак зодиака {current_sign}")
+
+
+def current_sign_info_int(request, current_sign_int):
+    if len(all_signs_dict) >= current_sign_int > 0:
+        url_link = reverse("current_sign_url", args=(list(all_signs_list)[current_sign_int - 1],))
+        return HttpResponseRedirect(url_link)
+    else:
+        return HttpResponseNotFound(f"Извините, но мы не знаем знак зодиака под номером {current_sign_int}")

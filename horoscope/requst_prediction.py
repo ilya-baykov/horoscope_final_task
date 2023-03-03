9534f6d7372f29c6bd078946b0c7dd74b19dace4
ilyab <i-baykov@internet.ru>
2023-03-03 17:05:20 +0300

    Внес изменения  класс Prediction, Теперь в случае какой нибудь ошибки будет возвращаться [False,False]. Добавил тег if в current_sign_info.html

--- horoscope/requst_prediction.py
+++ horoscope/requst_prediction.py
@@ -7,16 +7,18 @@ class Prediction:
     def __init__(self, current_sign: str):
         self.__current_sign = current_sign
         self.url = f"https://horo.mail.ru/prediction/{self.__current_sign}/today"
-
-    def request_info(self):
-        return requests.get(self.url, auth=('user', 'pass'))
+        self.request_info = requests.get(self.url, auth=('user', 'pass'))
 
     def treatment_info(self):
-        html_text = Prediction.request_info(self).text
+        html_text = self.request_info.text
         soup = BeautifulSoup(html_text, "lxml")
-        return soup.find('div',
-                         class_='article article_white article_prediction article_collapsed margin_top_20').find_all(
-            "p")
+        try:
+            test = soup.find('div',
+                             class_='article article_white article_prediction article_collapsed margin_top_20').find_all(
+                "p")
+            return test
+        except Exception:
+            return [False, False]
 
     @property
     def prediction(self):
@@ -25,3 +27,4 @@ class Prediction:
     @property
     def warning(self):
         return list(Prediction.treatment_info(self))[1]
+
--- horoscope/views.py
+++ horoscope/views.py
@@ -12,27 +12,14 @@ def menu(request):
 #
 def current_sign_info_str(request, current_sign):
     validation = all_signs_dict.get(current_sign)
-    if validation:
-        current_sign = Prediction(current_sign)
-        data = {
-            'current_sign': current_sign,
-            'validation': validation,
-            "prediction": str(current_sign.prediction),
-            "warning": str(current_sign.warning),
-        }
-        return render(request, 'horoscope/current_sign_info.html', context=data)
-    else:
-        return HttpResponseNotFound(f"Извините, но мы не знаем такой знак зодиака {current_sign}")
-
-
-# def display(current_sign, info: list):
-#     current_sign = Prediction(current_sign)
-#     res = f"<h1><div style='text-align: center;'>{info[1]}</div></h1><br>"
-#     res += f"<p><b>Советы на сегодня</b></p>"
-#     res += f"{current_sign.prediction}"
-#     res += f"<p><b>Будьте осторожней</b></p>"
-#     res += f"{current_sign.warning}"
-#     return HttpResponse(f"{res}")
+    current_sign = Prediction(current_sign)
+    data = {
+        'current_sign': current_sign,
+        'validation': validation,
+        "prediction": str(current_sign.prediction),
+        "warning": str(current_sign.warning),
+    }
+    return render(request, 'horoscope/current_sign_info.html', context=data)
 
 
 def current_sign_info_int(request, current_sign_int):

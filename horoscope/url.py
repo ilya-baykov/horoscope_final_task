9c15c53997e286a0ed49a102884237824491b293
ilyab <i-baykov@internet.ru>
2023-03-04 15:46:00 +0300

    Реализовал меню стихий знаков зодиака через HTML файл

--- horoscope/url.py
+++ horoscope/url.py
@@ -3,8 +3,8 @@ from horoscope import views as vs
 
 urlpatterns = [
     path('menu', vs.menu),
-    path('type', vs.type_menu, name="url_type"),
-    path('type/<str:current_type>', vs.current_type),
+    path('type/', vs.type_menu),
+    path('type/<str:current_type>', vs.current_type, name="url_type"),
     path("<int:current_sign_int>", vs.current_sign_info_int),
     path("<str:current_sign>", vs.current_sign_info_str, name="current_sign_url"),
     path("<int:month>/<int:day>", vs.time_search)

7d254d0e86bd44a6a2a00eeb67882e6b91d5a734
ilyab <i-baykov@internet.ru>
2023-03-05 17:29:57 +0300

    Добавил подскази к фунцкиям и прописал дополнительные 'тайпхитинги'

--- horoscope/support_function_views.py
+++ horoscope/support_function_views.py
@@ -1,16 +1,16 @@
-from django.http import HttpResponse
-from django.urls import reverse
 from horoscope.support_cls import ZodiacSign
 from horoscope.data_time_cls import DataTime
 
 all_signs_dict = ZodiacSign.all_sign
 all_signs_list = list(ZodiacSign.all_sign)
+
 signs_type_en_ru = {
     "fire": "Огня",
     "earth": "Земли",
     "air": "Воздушные",
     "water": "Водные"
 }
+
 MONTH_POSITION = dict(zip(
     [int(i) for i in range(1, 13)],
     ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь",
@@ -20,23 +20,14 @@ month_quantity_dict = DataTime.data_month_info
 
 
 def search_sign_by_date(month: int, day: int, quantity: int) -> str | tuple:
+    """ Проверяет коректность переданных параметров и вызывает вспомогательные функции """
     if 12 >= month > 0 and quantity >= day > 0:
         return time_search_sign(month, day)
     else:
         return f"Дата введена неверно - {day} {month}"
 
 
-def display_date_search(sign, month, day):
-    """ Выводит на экран ссылку на нужный знак зодиака"""
-    url_link = reverse("current_sign_url", args=(sign,))
-    month_info = DataTime.data_month_info.get(month)
-    total_display = f"<h2>Месяц - {month_info[0]} ,<br> день - {day}</h2>"
-    sign_info = ZodiacSign.all_sign.get(sign)
-    total_display += f"<a href = '{url_link}'>{sign_info[1]}</a>"
-    return HttpResponse(f"{total_display}")
-
-
-def time_search_sign(month, day):
+def time_search_sign(month: int, day: int) -> tuple:
     """Определяет какой знак зодиака нужно вывести на экран"""
     day_of_the_year_result = day_of_the_year(month) + day
     intervals = ZodiacSign.intervals
@@ -45,12 +36,9 @@ def time_search_sign(month, day):
         if verdict:
             return sign, month, day
     return "capricorn", month, day
-    #     if verdict:
-    #         return display_date_search(sign, month, day)
-    # return display_date_search("capricorn", month, day)
 
 
-def day_of_the_year(month) -> int:
+def day_of_the_year(month: int) -> int:
     """ Суммирует количество дней предыдуших месяцев """
     result = 0
     current_month = 1
